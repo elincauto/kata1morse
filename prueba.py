a1437b3623a7f3d190234cf5d018cc73e69e8088
elincauto <victormcnz@gmail.com>
2019-06-17 23:02:46 +0200

    pruebas iniciales

---
+++ prueba.py
@@ -0,0 +1,19 @@
+
+#        012345678901234567890123456
+letras ="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+simbolos = ['·—','—···','—·—·','————','—··','·','··—·','——·','····','··','·———','—·—','·—··','——','—·','——·——','———','·——·','——·—','·—·','···','—','··—','···—','·——','—··—','—·——','——··','—————','·————','··———','···——','····—','·····','—····','——···','———··','————·','·—·—·—','—·—·——','··——··','·—··—·','——··——'] 
+cadena= "Hola, mundo".upper
+for letra in cadena:
+    posicion=0
+    while posicion < len(letras):
+        l = letras[posicion]
+        if l==letra:
+            break
+        posicion +=1
+    if posicion==len(letras):
+        print("no encontrado")
+    else:
+        #Obtener letas morse
+        pass
+        print("{}-{}".format(letra,simbolos[posicion]))
+

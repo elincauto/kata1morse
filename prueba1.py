1d7ecdd0affb1017c340255a2e36a9cecfca3942
elincauto <victormcnz@gmail.com>
2019-06-17 23:15:34 +0200

    pruebas2

--- prueba1.py
+++ prueba1.py
@@ -43,17 +43,9 @@ simbolos = {
     '"': '·—··—·',
     '!': '——··——'}
 
-cadena= "Hola, mundo".upper
+cadena= "Hola, mundo".upper()
 for letra in cadena:
-    posicion=0
-    while posicion < len(letras):
-        l = letras[posicion]
-        if l==letra:
-            break
-        posicion +=1
-    if posicion==len(letras):
-        print("no encontrado")
-    else:
-        #Obtener letas morse
-        pass
+    if letra in morse:
         print("{}-{}".format(letra,simbolos[posicion]))
+    else:
+         print("no encontrado")
